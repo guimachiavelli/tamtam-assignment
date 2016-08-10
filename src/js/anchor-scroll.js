641887a3a43f0c829df4b5c6e64b53f017f7f772
gui machiavelli <hey@guimachiavelli.com>
2016-08-10 15:03:10 +0200

    add smooth anchor scrolling

---
+++ src/js/anchor-scroll.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var jump = require('jump.js');
+
+var anchorScroll = {
+    init: function(el) {
+        this.el = el;
+        this.target = this.targetFromHREF(el.href);
+        this.bind();
+    },
+
+    bind: function() {
+        this.el.addEventListener('click', this.onAnchorClick.bind(this));
+    },
+
+    targetFromHREF: function(href) {
+        href = href.split('#');
+
+        return '#' + href[1];
+    },
+
+    onAnchorClick: function(event) {
+        var target;
+
+        event.preventDefault();
+
+        jump(this.target);
+    },
+};
+
+module.exports = anchorScroll;
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 var slider = require('./slider'),
-    nav = require('./nav');
+    nav = require('./nav'),
+    anchorScroll = require('./anchor-scroll');
 
 var app = {
 
     init: function() {
         slider.init();
         nav.init(document.querySelector('.site-nav'));
+        anchorScroll.init(document.querySelector('.hero__scroll-anchor'));
     }
 
 };
